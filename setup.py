5e2a3c2a69182c79016267d2b445732f621eb399
karthikvarma247 <karthikvarma247@gmail.com>
2023-09-11 17:34:43 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setup.tools import find_packages
+from typing import list
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+    name = "ML-project"
+    version = "1.0"
+    author = "karthik"
+    author_email = "karthikvarma247@gmail.com"
+    packages = find_packages()
+    intall_requires = get_requirements("requiremnts.txt")
+)
